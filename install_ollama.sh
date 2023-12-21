6ea08731350fe051eb1be0c3ccc911eab7ae3205
Administrator <deadc0de@centerionware.com>
2023-12-21 06:45:34 +0000

    Update file install_ollama.sh

--- install_ollama.sh
+++ install_ollama.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 curl https://ollama.ai/install.sh | sh
 /usr/local/bin/ollama serve &
+sleep 1s
+echo "Pulling mistral"
 ollama pull mistral
+echo "Pulling llava"
 ollama pull llava
 killall ollama -9
